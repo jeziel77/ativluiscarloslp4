5e5f36048c5ee5db56860d06ad762a6e2fff61ea
jeziel77 <jeziel6688@gmail.com>
2021-04-25 16:04:03 -0300

    atividade

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// Se você deseja começar a medir o desempenho em seu aplicativo, passe uma função
+// para registrar os resultados (por exemplo: reportWebVitals (console.log))
+// ou envie para um endpoint analítico. Saiba mais: https://bit.ly/CRA-vitals
+reportWebVitals();
